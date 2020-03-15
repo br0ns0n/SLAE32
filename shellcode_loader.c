c38e7c93afe1ad67e61fbb382275e9c7f6acf618
br0ns0n <bbmartin01@gmail.com>
2020-03-15 16:40:00 -0700

    Create shellcode_loader.c

---
+++ shellcode_loader.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+
+unsigned char code[] = \
+"INSERT SHELLCODE HERE"
+main()
+{
+
+	printf("Shellcode Length:  %d\n", strlen(code));
+
+	int (*ret)() = (int(*)())code;
+
+	ret();
+
+}
